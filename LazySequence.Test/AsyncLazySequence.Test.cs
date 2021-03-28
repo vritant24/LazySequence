a9eead2eb5d25675c306992c22e6a6a158ceaf12
Vritant Bhardwaj <vrbhardw@microsoft.com>
2021-03-28 11:24:32 -0700

    added tests

---
+++ LazySequence.Test/AsyncLazySequence.Test.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LazySequence.Test
+{
+    [TestClass]
+    public class AsyncLazySequence
+    {
+        [TestMethod]
+        [DataRow(0, false)]
+        [DataRow(null, true)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ContructorShouldThrowForNullArgs(
+           int? firstElement,
+           bool getNextElement)
+        {
+            if (getNextElement)
+            {
+                _ = AsyncLazySequence<int?>.Create(
+                    firstElement, (i, k) => Task.FromResult((i, false)));
+            }
+            else
+            {
+                _ = AsyncLazySequence<int?>.Create(
+                    firstElement, null);
+            }
+        }
+
+        [TestMethod]
+        public async Task ShouldReturnCorrectSequenceAsync()
+        {
+            var x = AsyncLazySequence<int>.Create(
+                    1, (i, k) => Task.FromResult((i + 1, false))); ;
+
+            Assert.AreEqual(1, await x.FirstAsync());
+            Assert.AreEqual(2, await x.Skip(1).FirstAsync());
+            Assert.AreEqual(3, await x.Skip(2).FirstAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldUpdateIndexAsync()
+        {
+            var x = AsyncLazySequence<int>.Create(
+                    0, (_, k) => Task.FromResult(((int)k, false)));
+
+            Assert.AreEqual(1, await x.Skip(1).FirstAsync());
+            Assert.AreEqual(2, await x.Skip(2).FirstAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldCompleteAsync()
+        {
+            var x = AsyncLazySequence<int>.Create(
+                    1, (i, _) => Task.FromResult((i + 1, i == 10)));
+
+            Assert.AreEqual(10, await x.CountAsync());
+        }
+    }
+}
---
+++ LazySequence.Test/AsyncStatefulLazySequenceTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LazySequence.Test
+{
+    [TestClass]
+    public class AsyncStatefulLazySequenceTest
+    {
+        [TestMethod]
+        [DataRow(0, 0, false)]
+        [DataRow(null, 0, true)]
+        [DataRow(0, null, true)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ContructorShouldThrowForNullArgs(
+            int? firstElement,
+            int? initialState,
+            bool getNextElement)
+        {
+            if (getNextElement)
+            {
+                _ = AsyncLazySequence<int?, int?>.Create(
+                    firstElement, initialState, (i, j, k) => Task.FromResult((i, j, false)));
+            }
+            else
+            {
+                _ = AsyncLazySequence<int?, int?>.Create(
+                    firstElement, initialState, null);
+            }
+        }
+
+        [TestMethod]
+        public async Task ShouldReturnCorrectSequenceAsync()
+        {
+            var x = AsyncLazySequence<int, int>.Create(
+                    1, 0, (i, _, _) => Task.FromResult((i + 1, 0, false)));
+
+            Assert.AreEqual(1, await x.FirstAsync());
+            Assert.AreEqual(2, await x.Skip(1).FirstAsync());
+            Assert.AreEqual(3, await x.Skip(2).FirstAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldUpdateStateAsync()
+        {
+            var x = AsyncLazySequence<int, int>.Create(
+                    1, 0, (i, j, _) => Task.FromResult((i + j, 1, false)));
+
+            Assert.AreEqual(1, await x.Skip(1).FirstAsync());
+            Assert.AreEqual(2, await x.Skip(2).FirstAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldUpdateIndexAsync()
+        {
+            var x = AsyncLazySequence<int, int>.Create(
+                    0, 0, (_, _, k) => Task.FromResult(((int)k, 0, false)));
+
+            Assert.AreEqual(1, await x.Skip(1).FirstAsync());
+            Assert.AreEqual(2, await x.Skip(2).FirstAsync());
+        }
+
+        [TestMethod]
+        public async Task ShouldCompleteAsync()
+        {
+            var x = AsyncLazySequence<int, int>.Create(
+                    1, 1, (i, j, _) => Task.FromResult((i + j, 1, i == 10)));
+
+            Assert.AreEqual(10, await x.CountAsync());
+        }
+    }
+}
